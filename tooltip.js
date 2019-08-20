392090db0fc3375f4cc03282bcf6d26d8e93e6f9
Wieczoreks <wieczoreks@hotmail.com>
2019-08-20 22:11:37 +0200

    added flip functionality for right-left and top-bottom

--- tooltip.js
+++ tooltip.js
@@ -7,7 +7,6 @@ document.addEventListener('DOMContentLoaded', () => {
    
     for(var i=0; i< spans.length; i++){
         let span = spans[i];
-       
         span.addEventListener('mouseover', (e) => {
         let textNode = span.getAttribute('data-tooltip')
         let tooltip = document.createElement('div')
@@ -16,48 +15,71 @@ document.addEventListener('DOMContentLoaded', () => {
         let firstChild = document.body.firstChild;//gets the first elem after body
         firstChild.parentNode.insertBefore(tooltip, firstChild);
         
-        let padding = 5
+        let padding = 5;
         let spanBox = span.getBoundingClientRect();
         let tooltipBox = tooltip.getBoundingClientRect();   
         let topPos;
         let leftPos;
         let position = span.getAttribute("data-position");
-        let pageYOffset = window.pageYOffset
+        let pageYOffset = window.pageYOffset;
+        let pageXOffset = window.pageXOffset
         
 
         if (position === "top") {
+            
             topPos = pageYOffset + spanBox.top - (tooltipBox.height + padding);
             leftPos = spanBox.left
             tooltip.classList.add('top')
             tooltip.setAttribute('style', 'top:'+topPos+'px;'+'left:'+leftPos+ 'px')
             
+            if(tooltip.getBoundingClientRect().top <= 0 ){
+                topPos = pageYOffset + spanBox.top + (spanBox.height + padding);
+                tooltip.classList.remove('top');
+                tooltip.classList.add('bottom');
+                tooltip.setAttribute('style', 'top:'+topPos+'px;'+'left:'+leftPos+ 'px')
+            }
         }
         else if (position === "bottom") {
+                let tooltipBox;
                 leftPos = spanBox.left;
                 topPos = pageYOffset + spanBox.top + (spanBox.height + padding);
                 tooltip.classList.add('bottom');
                 tooltip.setAttribute('style', 'top:'+topPos+'px;'+'left:'+leftPos+ 'px')
-             if(topPos+tooltipBox.height > docHeight+pageYOffset){
-                span.setAttribute("data-position","top");
-                topPos = pageYOffset + spanBox.top - (tooltipBox.height + padding);
-                tooltip.classList.add('top')
-                tooltip.setAttribute('style', 'top:'+topPos+'px;'+'left:'+leftPos+ 'px')
-                console.log("TEST" )
-             } 
-             tooltip.classList.remove('top')
+                tooltipBox = tooltip.getBoundingClientRect();
+                if(tooltipBox.bottom >= docHeight){
+                    topPos = pageYOffset + spanBox.top - (tooltipBox.height + padding);
+                    tooltip.classList.remove('bottom');
+                    tooltip.classList.add('top');
+                    tooltip.setAttribute('style', 'top:'+topPos+'px;'+'left:'+leftPos+ 'px')
+                }
+
         }
         else if (position === "left") {
             topPos = pageYOffset + spanBox.top + padding;
-            leftPos = spanBox.left - (tooltipBox.width + padding)
-            
+            leftPos = pageXOffset+ spanBox.left - (tooltipBox.width + padding)
             tooltip.classList.add('left');
-            tooltip.setAttribute('style', 'top:'+topPos+'px;'+'left:'+leftPos+ 'px')
+            tooltip.setAttribute('style', 'top:'+topPos+'px;'+'left:'+leftPos+ 'px');
+            if(tooltip.getBoundingClientRect().left<=0){
+                
+                topPos = pageYOffset + spanBox.top + padding;
+                leftPos = pageXOffset+ spanBox.left + (spanBox.width + padding)
+                tooltip.classList.remove('left');
+                tooltip.classList.add('right');
+                tooltip.setAttribute('style', 'top:'+topPos+'px;'+'left:'+leftPos+ 'px')
+            }
         }
         else if (position === "right") {
             topPos = pageYOffset + spanBox.top + padding;
-            leftPos = spanBox.left + (spanBox.width + padding)
+            leftPos =pageXOffset+ spanBox.left + (spanBox.width + padding)
             tooltip.classList.add('right');
-            tooltip.setAttribute('style', 'top:'+topPos+'px;'+'left:'+leftPos+ 'px')
+            tooltip.setAttribute('style', 'top:'+topPos+'px;'+'left:'+leftPos+ 'px');
+            if(tooltip.getBoundingClientRect().left <= docWidth){
+            topPos = pageYOffset + spanBox.top + padding;
+            leftPos = pageXOffset+ spanBox.left - (tooltipBox.width + padding);
+            tooltip.classList.remove('right');
+            tooltip.classList.add('left');
+            tooltip.setAttribute('style', 'top:'+topPos+'px;'+'left:'+leftPos+ 'px');
+            }
         } 
         else {
             topPos = spanBox.top - (tooltipBox.height + padding);
